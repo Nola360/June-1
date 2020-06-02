01fb1b5a9342746a56743e20e8c5881ca21e64ca
nola360 <adaramolajr@gmail.com>
2020-06-02 03:05:32 -0400

    added clear field and alert message ui

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // Book Constructor
-
 function Book(title, author, isbn) {
   this.title = title;
   this.author = author;
@@ -15,22 +14,51 @@ function UI() {
 
 
 UI.prototype.addBookToList = function (book) {
+  // Targets html table
   let list = document.querySelector('#book_list');
 
+  // Constructs row for books
   const row = document.createElement('tr');
-
   row.innerHTML = `
   <td>${book.title}</td>
   <td>${book.author}</td>
   <td>${book.isbn}</td>
   <td><i class="fas fa-times"></td>
   `;
-
   list.appendChild(row);
-
-
-
 }
+
+UI.prototype.clearFields = function () {
+  const title = document.querySelector('#title').value = '',
+    author = document.querySelector('#author').value = '',
+    isbn = document.querySelector('#isbn').value = '';
+}
+
+UI.prototype.showAlert = function (message, className) {
+  // Constructing div element
+  const message_box = document.createElement('div');
+
+  // Add ClassName
+  message_box.className = `alert ${className}`;
+
+  // Appending text to message 
+  message_box.appendChild(document.createTextNode(message))
+
+  // Targeting container
+  let container = document.querySelector('.container');
+
+  // Target book form
+  let form = document.querySelector('#book_form');
+
+  // Inserting message before form
+  container.insertBefore(message_box, form);
+
+  // Time out After 3 seconds
+  setTimeout(function () {
+    document.querySelector('.alert').remove()
+  }, 3000);
+}
+
 
 // Event Listners
 document.getElementById('book_form').addEventListener('submit', function (e) {
@@ -40,13 +68,30 @@ document.getElementById('book_form').addEventListener('submit', function (e) {
     author = document.querySelector('#author').value,
     isbn = document.querySelector('#isbn').value;
 
+  // Book Instanciation
   const book = new Book(title, author, isbn);
 
+  // UI instanciation
   const ui = new UI();
 
-  ui.addBookToList(book);
+
+  if (title === '' || author === '' || isbn === '') {
+    // Error
+    ui.showAlert('Fields cannot be left empty.', 'error');
+
+  } else {
+
+    // Adds book to list via method in UI constructor
+    ui.addBookToList(book);
+
+    // Clear Fields
+    ui.clearFields();
+
+    ui.showAlert('Book added!', 'success');
+  }
 
 
-  console.log(book);
+  // console.log(book);
   e.preventDefault();
 })
+
